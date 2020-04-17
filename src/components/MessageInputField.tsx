573df2b9824389fca2f28c215c1ccc0f816d49f2
jen <jengkarlong@gmail.com>
2020-04-17 22:18:45 +0800

    add styles on input send button

--- src/components/MessageInputField.tsx
+++ src/components/MessageInputField.tsx
@@ -8,8 +8,8 @@ const MessageInputField = (props: any) => {
 
     return (
         <div className="chatpack__input__container flex justify-center">
-            <input autoFocus className="chatpack__input__field rounded-sm py-4 px-5 text-center rounded-lg shadow-md" type={type} />
-            <button className="chatpack__input__send">
+            <input autoFocus className="chatpack__input__field rounded-sm py-4 px-5 text-center rounded rounded-r-none shadow-md" type={type} />
+            <button className="chatpack__input__send py-4 px-5 rounded-lg rounded-l-none shadow-md">
                 <FontAwesomeIcon icon="paper-plane" />
             </button>
         </div>
